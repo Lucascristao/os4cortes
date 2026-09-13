aa260b4af45cdfabee5eda28f5143d578d17f5c4
Lucas Freitas <lucasgoodtech@gmail.com>
2026-09-13 15:15:56 -0300

    Atualiza cache para persistência do último trabalho

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "os4-cortes-v6";
+const CACHE = "os4-cortes-v7";
 const APP_SHELL = ["/", "/index.html", "/style.css", "/app.js", "/enhancements.js", "/manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
